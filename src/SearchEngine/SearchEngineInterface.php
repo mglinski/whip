184bec7517183a60aa0e9f59a9bd670820537abd
Victor Lantigua <vmlantigua@gmail.com>
2017-10-06 12:42:37 -0400

    Add interface for search engine

---
+++ src/SearchEngine/SearchEngineInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Slicvic\Whip\SearchEngine;
+
+/**
+ * Interface for search engine.
+ */
+interface SearchEngineInterface
+{
+    /**
+     * Find images by keywords.
+     *
+     * @param  array   $keywords   A list of keywords to match.
+     * @param  integer $maxResults Maximum number of results to return.
+     * @return \Slicvic\Whip\SearchEngine\SearchResult
+     */
+    public function search(array $keywords, int $maxResults = 3);
+}
